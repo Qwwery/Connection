cfb1a27b18915890c8af62fef95e5fafcb0e207a
Qwwery <151052884+Qwwery@users.noreply.github.com>
2024-11-02 03:22:38 +0300

    added the ability to register

--- main.py
+++ main.py
@@ -1,19 +1,110 @@
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for, sessions, make_response, jsonify
+from models import db_session
+from models.users import User
+from flask_login import login_user, logout_user, login_required, current_user
 
 app = Flask(__name__)
 app.secret_key = 'DOTA 2'
+db_session.global_init('db/db.db')
 
 
 def main():
     app.run(debug=True)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def start():
-    if not session.get('logged_in'):
-        return render_template('hello.html')
+    if request.method == 'GET':
+        if not session.get('logged_in'):
+            return render_template('hello.html')
+        else:
+            return redirect(url_for('feed'))
+    elif request.method == 'POST':
+        db_sess = db_session.create_session()
+
+        if request.form.get('button') == 'registration':
+            form_email = request.form.get('email')
+            form_password = request.form.get('password')
+            session['email'] = form_email
+            session['password'] = form_password
+
+            if db_sess.query(User).filter(User.email == form_email).first():
+                return redirect(url_for('authorization'))
+
+            return redirect(url_for('registration'))
+        else:
+            form_login = request.form.get('login')
+            form_password = request.form.get('password')
+            session['login'] = form_login
+            session['password'] = form_password
+            return redirect(url_for('authorization'))
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if session.get('logged_in'):
+        return redirect(url_for('feed'))
+    if request.method == 'GET':
+        info = {
+            'email': session.get('email'),
+            'password': session.get('password')
+        }
+        return render_template('registration.html', info=info)
     else:
-        return render_template('feed.html')
+        # db_sess = db_session.create_session()
+        # form_name = request.form.get('name')
+        # form_last_name = request.form.get('lastName')
+        # form_login = request.form.get('login')
+        # form_email = request.form.get('email')
+        # form_password = request.form.get('password')
+
+        # check = db_sess.query(User).filter(User.email == form_email).first()
+
+        return redirect(url_for('authorization'))
+
+
+@app.route('/check-email', methods=['POST'])
+def check_email():
+    db_sess = db_session.create_session()
+    data = request.json
+    email = data.get("email")
+    exists = db_sess.query(User).filter(User.email == email).first()
+    if exists:
+        return jsonify({"exists": True})
+    else:
+        return redirect(url_for('registration'))
+
+
+@app.route('/registration-new-user', methods=['POST'])
+def register_new_user():
+    db_sess = db_session.create_session()
+    data = request.json
+    name = data.get("name")
+    email = data.get("email")
+    last_name = data.get("lastName")
+    password = data.get("password")
+    login = data.get("login")
+    print(name, email, last_name, password, login)
+    user = User(name=name, email=email, last_name=last_name, special_login=login)
+    user.set_password(password)
+    db_sess.add(user)
+    db_sess.commit()
+    return 'ok'
+
+
+@app.route('/authorization', methods=['GET', 'POST'])
+def authorization():
+    return render_template('authorization.html')
+
+
+@app.route('/privacy', methods=['GET', 'POST'])
+def privacy():
+    return render_template('privacy.html')
+
+
+@app.route('/feed', methods=['GET', 'POST'])
+def feed():
+    return render_template('feed.html')
 
 
 if __name__ == '__main__':
---
+++ models/users.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+
+    special_login = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    last_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    def __repr__(self):
+        return f'<User> id:{self.id} name:{self.name} login:{self.special_login} email:{self.email}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
